3fa64655aad9384f7d557022ac549b652fd77aad
Rohit <rohit.truesdale@gmail.com>
2025-04-20 11:57:20 -0700

    updated sqlite dependency version and it's initialization in the __init__.py

--- feature-WebScraperStockSelector/app/__init__.py
+++ feature-WebScraperStockSelector/app/__init__.py
@@ -1,18 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-# Initialize Flask app
-app = Flask(__name__)
-
-# Initialize database
+# Initialize extensions
 db = SQLAlchemy()
 
 def create_app():
+    # Initialize Flask app
+    app = Flask(__name__)
+    
     # Configure the Flask app
     app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://{app.config['MYSQL_USER']}:{app.config['MYSQL_PASSWORD']}@{app.config['MYSQL_HOST']}/{app.config['MYSQL_SCRAPING_DATABASE']}"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
-    # Initialize extensions
+    # Initialize extensions with app
     db.init_app(app)
     
     # Import and register blueprints
