53e337d2e36a41b1067e44a6cddb0428267a30b6
Rohit <rohit.truesdale@gmail.com>
2025-04-30 18:56:29 -0700

    have to correct the port number on the app.py

--- feature-TradeLogistics/app.py
+++ feature-TradeLogistics/app.py
@@ -247,5 +247,5 @@ def health():
     return jsonify(status="healthy")
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5009))
+    port = int(os.getenv('PORT', 5008))
     app.run(host='0.0.0.0', port=port)
