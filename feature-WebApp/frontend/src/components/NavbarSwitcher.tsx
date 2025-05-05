1039a88b1404765f41f77192e9335095d2239bf9
Rohit <rohit.truesdale@gmail.com>
2025-05-05 04:24:31 -0700

    Remove all references to adminTab and setAdminTab in NavbarSwitcher.tsx.

--- feature-WebApp/frontend/src/components/NavbarSwitcher.tsx
+++ feature-WebApp/frontend/src/components/NavbarSwitcher.tsx
@@ -4,15 +4,13 @@ import ExternalNavbar from './ExternalNavbar';
 import InternalNavbar from './InternalNavbar';
 import AdminNavbar from './AdminNavbar';
 import { usePathname } from 'next/navigation';
-import { useState } from 'react';
 
 export default function NavbarSwitcher() {
   const pathname = usePathname();
-  const [adminTab, setAdminTab] = useState(0);
 
   if (!pathname) return null;
   if (pathname.startsWith('/admin-dashboard')) {
-    return <AdminNavbar activeIndex={adminTab} setActiveIndex={setAdminTab} />;
+    return <AdminNavbar />;
   }
   if (pathname.startsWith('/dashboard')) {
     return <InternalNavbar />;
